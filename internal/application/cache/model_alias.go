c4e38067ed0acc2e6d701823ec9b2ce6a8b7a470
useryege <niandizhuandaqian@gmail.com>
2026-06-06 14:46:27 +0800

    commit

--- internal/application/cache/model_alias.go
+++ internal/application/cache/model_alias.go
@@ -6,6 +6,3 @@ type Project = model.Project
 type ProjectMeta = model.ProjectMeta
 type SimulateResult = model.SimulateResult
 type GenesisWalletMeta = model.GenesisWalletMeta
-type ProjectAveDetail = model.ProjectAveDetail
-type ProjectAveTokenDetail = model.ProjectAveTokenDetail
-type ProjectAvePair = model.ProjectAvePair
