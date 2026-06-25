328bda7dee01ced3a870763e2de69bf8780d43ab
useryege <niandizhuandaqian@gmail.com>
2026-06-25 14:21:47 +0800

    commit

--- internal/polymarket/managed_oo_disputed_alerts.go
+++ internal/polymarket/managed_oo_disputed_alerts.go
@@ -123,7 +123,7 @@ func (s *Service) renderManagedOODisputePriceAlertNotification(candidate polymar
 		Title:        fmt.Sprintf("UMA Disputed: %s", truncateRunes(titleSubject, defaultManagedOODisputedAlertTitleQuestionRunes)),
 		Body:         strings.Join(bodyLines, "\n"),
 		Link:         s.managedOODisputePriceAlertLink(candidate),
-		TelegramChat: notificationapiclient.TelegramChat_TELEGRAM_CHAT_TEST,
+		TelegramChat: notificationapiclient.TelegramChat_TELEGRAM_CHAT_PROD,
 		TopicLabel:   managedOODisputedAlertTopic,
 	}
 }
